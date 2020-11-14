c54f736bb2b708a7b3ea22aaf9621c94e1b244df
jmargielewski <kuba.margielewski@gmail.com>
2020-11-14 20:16:53 +0000

    container init

---
+++ packages/container/src/components/MarketingApp.js
@@ -0,0 +1,13 @@
+// 'marketing' has to match up with key in remotes object of ModuleFederationPlugin
+import { mount } from 'marketing/MarketingApp';
+import React, { useRef, useEffect } from 'react';
+
+export default () => {
+  const ref = useRef(null);
+
+  useEffect(() => {
+    mount(ref.current);
+  });
+
+  return <div ref={ref} />;
+};
