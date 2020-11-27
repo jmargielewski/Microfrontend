14215c17d95644f7a8b1b5e58b75652d5ac293c0
jmargielewski <kuba.margielewski@gmail.com>
2020-11-27 21:43:46 +0000

    added dashboard

---
+++ packages/container/src/components/DashboardApp.js
@@ -0,0 +1,13 @@
+// 'marketing' has to match up with key in remotes object of ModuleFederationPlugin
+import { mount } from 'dashboard/DashboardApp';
+import React, { useRef, useEffect } from 'react';
+
+export default () => {
+  const ref = useRef(null);
+
+  useEffect(() => {
+    mount(ref.current);
+  }, []);
+
+  return <div ref={ref} />;
+};
